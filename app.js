63adc8dfeb2555c3061aa66933342c59d9f10049
Chely Ho <adept_inept@yahoo.com>
2017-11-20 18:15:51 +0000

    automatically backed up by learn

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var express = require('express')
 var app = express()
 
-// Build Your Route Here
+app.get('/', function(req,res){
+  res.send('Hello, World!')
+})
 
-
-module.exports = app
+module.exports = app()
